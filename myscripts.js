3d717a26facb45a3e00c215c9561c17014e0ae01
Rafay Khawar <rafaykhawar09@gmail.com>
2019-11-26 23:28:38 -0800

    uploading project

---
+++ myscripts.js
@@ -0,0 +1,31 @@
+
+//toggle navbar
+$(document).ready(function(){
+     $(".hamburger").click(function(){
+          console.log(this)
+          if($(this).hasClass("closed")){
+               $(".nav-group").slideDown();
+               $(".hamburger").removeClass("closed");
+               $(".hamburger").addClass("open");
+          }
+          else if($(this).hasClass("open")){
+               $(".nav-group").slideUp();
+               $(".hamburger").removeClass("open");
+               $(".hamburger").addClass("closed");
+          }
+     });
+});
+
+//after scroll a certain height
+//change background-color of
+//navbar
+$(window).scroll(function(){
+     if($(this).scrollTop() > 50){
+          $(".nav").removeClass("nav-bkg-before-scroll");
+          $(".nav").addClass("nav-bkg-after-scroll");
+     }
+     else{
+          $(".nav").removeClass("nav-bkg-after-scroll");
+          $(".nav").addClass("nav-bkg-before-scroll");
+     }
+});
